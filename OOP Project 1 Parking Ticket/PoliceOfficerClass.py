54d05026b574f2c39a094f79d03b315e44c1a99d
Jeremiah Penney <Jeremiahpenney9@gmail.com>
2025-09-21 16:16:54 -0400

    added class constructor for police officer and f string for output

--- OOP Project 1 Parking Ticket/PoliceOfficerClass.py
+++ OOP Project 1 Parking Ticket/PoliceOfficerClass.py
@@ -1,2 +1,9 @@
 class PoliceOfficerClass:
     "Police officer who issues tickets."
+
+def __init__(self, name: str, badge_number: str):
+    self.name = name
+    self.badge_number = badge_number
+
+def __str__(self) -> str:
+    return f"Officer {self.name} (Badge {self.badge_number})"
