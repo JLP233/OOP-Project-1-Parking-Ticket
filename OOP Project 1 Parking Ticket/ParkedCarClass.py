027f61fe858fbd31f06c252d646170749a6c645a
Jeremiah Penney <Jeremiahpenney9@gmail.com>
2025-09-21 15:44:27 -0400

    added constructor defintion for parked car class

--- OOP Project 1 Parking Ticket/ParkedCarClass.py
+++ OOP Project 1 Parking Ticket/ParkedCarClass.py
@@ -1,2 +1,10 @@
 class ParkedCarClass:
     "Identifying info for parked car."
+
+def __init__(self, make: str, model: str, color: str, license_number: str, minutes_parked: int = 60):
+    self.make = make
+    self.model = model
+    self.color = color
+    self.license_number = license_number
+    self.minutes_parked = minutes_parked
+
