e638824ab3a6da436de3b09a9dd305d0848694b5
Jeremiah Penney <Jeremiahpenney9@gmail.com>
2025-09-21 15:29:19 -0400

    added parking meter constructor and @property

--- OOP Project 1 Parking Ticket/ParkingMeterClass.py
+++ OOP Project 1 Parking Ticket/ParkingMeterClass.py
@@ -1,2 +1,9 @@
 class ParkingMeterClass:
     "Purhcased time from parking Meter."
+
+def __init__(self, minutes_purchased: int = 60):
+        self.minutes_purchased = minutes_purchased
+
+@property
+def minutes_purchased(self) -> int:
+        return self._minutes_purchased
