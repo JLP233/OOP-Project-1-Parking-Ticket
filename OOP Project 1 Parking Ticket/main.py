fff0e2726966f8eba4285d16632c3d605153211c
Jeremiah Penney <Jeremiahpenney9@gmail.com>
2025-09-21 15:09:46 -0400

    defined parking meter class

--- OOP Project 1 Parking Ticket/main.py
+++ OOP Project 1 Parking Ticket/main.py
@@ -1,1 +1,14 @@
+from ParkedCarClass import ParkedCarClass
+from ParkingMeterClass import ParkingMeterClass
+from PoliceOfficerClass import PoliceOfficerClass
 
+def run_and_print(car, meter, officer, title: str):
+    print(f"\n {title}")
+    print(f"Car: {car}")
+    print(f"Meter: {meter}")
+    print(f"Officer: {officer}")
+    ticket = officer.inspect_car(car, meter)
+    if ticket:
+        print("Result: Ticket Issued \n" + str(ticket))
+    else:
+        print("Result: Ticket not issued / Car parked legally.")
