dce1cea31cc90b1395257ae9f4a7633817335916
Jeremiah Penney <Jeremiahpenney9@gmail.com>
2025-09-21 17:24:53 -0400

    fixed class indendent

--- OOP Project 1 Parking Ticket/ParkedCarClass.py
+++ OOP Project 1 Parking Ticket/ParkedCarClass.py
@@ -1,7 +1,8 @@
 class ParkedCarClass:
     "Identifying info for parked car."
 
-def __init__(self, make: str, model: str, color: str, license_number: str, minutes_parked: int = 60):
+def __init__(self, make: str, model: str, color: str, 
+             license_number: str, minutes_parked: int = 60):
     self.make = make
     self.model = model
     self.color = color
