e8b206e7e745a7150e7de6a2ee13ebbd7e000c57
Jeremiah Penney <Jeremiahpenney9@gmail.com>
2025-09-21 17:51:13 -0400

    fixed class indendentation

--- OOP Project 1 Parking Ticket/ParkedCarClass.py
+++ OOP Project 1 Parking Ticket/ParkedCarClass.py
@@ -1,25 +1,26 @@
 class ParkedCarClass:
     "Identifying info for parked car."
 
-def __init__(self, make: str, model: str, color: str, 
-             license_number: str, minutes_parked: int = 60):
-    self.make = make
-    self.model = model
-    self.color = color
-    self.license_number = license_number
-    self.minutes_parked = minutes_parked
+    def __init__(self, make: str, model: str, color: str, 
+                 license_number: str, minutes_parked: int = 60):
+        self.make = make
+        self.model = model
+        self.color = color
+        self.license_number = license_number
+        self.minutes_parked = minutes_parked
 
-@property
-def minutes_parked(self) -> int:
-    return self._minutes_parked
+    @property
+    def minutes_parked(self) -> int:
+        return self._minutes_parked
 
-@minutes_parked.setter
-def minutes_parked(self, minutes: int) -> None:
-        if not isinstance(minutes, int):
-            raise TypeError("minutes_parked must be a whole number")
-        if minutes <= 0:
-            raise ValueError("minutes_parked cannot be less than 0")
-        self._minutes_parked = minutes
+    @minutes_parked.setter
+    def minutes_parked(self, minutes: int) -> None:
+            if not isinstance(minutes, int):
+                raise TypeError("minutes_parked must be a whole number")
+            if minutes <= 0:
+                raise ValueError("minutes_parked cannot be less than 0")
+            self._minutes_parked = minutes
 
-def __str__(self) -> str:
-    return f"{self.color} {self.make} {self.model} ({self.license_number}), parked {self.minutes_parked} min"
+    def __str__(self) -> str:
+        return f"{self.color} {self.make} {self.model} ({self.license_number}), parked {self.minutes_parked} min"
+
