3aa7e28affab3f07f2d5809caedd0cdba119345a
Jeremiah Penney <Jeremiahpenney9@gmail.com>
2025-09-21 17:42:13 -0400

    trying to debug

--- OOP Project 1 Parking Ticket/main.py
+++ OOP Project 1 Parking Ticket/main.py
@@ -1,3 +1,10 @@
+import os, sys, inspect, ParkedCarClass  # <-- the MODULE, not the class
+
+print("CWD        :", os.getcwd())
+print("USING FILE :", ParkedCarClass.__file__)
+print("INIT SIG   :", inspect.signature(ParkedCarClass.ParkedCarClass.__init__))
+print()
+
 from ParkedCarClass import ParkedCarClass
 from ParkingMeterClass import ParkingMeterClass
 from PoliceOfficerClass import PoliceOfficerClass
